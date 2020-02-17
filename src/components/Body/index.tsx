353b0d75ee2868a6abd5aa0d6387d458d4a494bf
Pozdnya <kika_domination@yahoo.com>
2020-02-17 23:23:18 +0200

    use class, state

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -3,47 +3,88 @@ import logo from './logo.svg';
 import style from './style.module.css'
 import keyboard from "./keyboard.svg"
 import microphone from './microphone.svg'
-// interface ownProps{
-// 	logo:string
-// }
 
-let Body: React.FC = () => {
-	const [val, setVal] = React.useState('')
-	const reg = "^[A-zА-яЁё]+$";
-	return (
-		<>
-			<div className={style.input}>
-				<div className={style.logo}>
-					<a href="https://google.com">
-						<img src={logo} alt="" />
-					</a>
-				</div>
-				<div className={style.inputBlock}>
-					<div className={style.inputDiv}>
-						<input type="text" className={style.input}
-							value={val}
-							onChange={(event) => {
-								event.target.value.match(reg) ? setVal(event.target.value) : setVal('')
-							}
-							} />
-								<div className={style.buttonsIcon}>
-						<button id='kb' className={style.inputButton}>
-							<img src={keyboard} alt="" className={style.img} />
-						</button>
-						<button id='mph' className={style.inputButton}>
-							<img src={microphone} alt="" className={style.img} />
-						</button>
-					</div>
-					</div>
-				</div>
-				<div className={style.buttons}>
-					<button className={style.button}>Посик в Google</button>
-					<button className={style.button}>Мне повезет</button>
-				</div>
-			</div>
-		</>
-	)
+
+interface ownProps {
+	inputValue: string
 }
+
+class Body extends React.Component<{}, ownProps>{
+	state = { inputValue: '' };
+	render() {
+		const reg = "^[A-zА-яЁё]+$";
+		return (
+			<>
+				<div className={style.input}>
+					<div className={style.logo}>
+						<a href="https://google.com">
+							<img src={logo} alt="" />
+						</a>
+					</div>
+					<div className={style.inputBlock}>
+						<div className={style.inputDiv}>
+							<input type="text" className={style.input}
+								value={this.state.inputValue}
+								onChange={(event) => {
+									event.target.value.match(reg) ? this.setState({ inputValue: event.target.value }) : this.setState({ inputValue: '' })
+								}
+								} />
+							<div className={style.buttonsIcon}>
+								<button id='kb' className={style.inputButton}>
+									<img src={keyboard} alt="" className={style.img} />
+								</button>
+								<button id='mph' className={style.inputButton}>
+									<img src={microphone} alt="" className={style.img} />
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className={style.buttons}>
+						<button className={style.button}>Посик в Google</button>
+						<button className={style.button}>Мне повезет</button>
+					</div>
+				</div>
+			</>)
+	}
+}
+
+// let Body: React.FC = () => {
+// 	const [val, setVal] = React.useState('')
+// 	const reg = "^[A-zА-яЁё]+$";
+// 	return (
+// 		<>
+// 			<div className={style.input}>
+// 				<div className={style.logo}>
+// 					<a href="https://google.com">
+// 						<img src={logo} alt="" />
+// 					</a>
+// 				</div>
+// 				<div className={style.inputBlock}>
+// 					<div className={style.inputDiv}>
+// 						<input type="text" className={style.input}
+// 							value={val}
+// 							onChange={(event) => {
+// 								event.target.value.match(reg) ? setVal(event.target.value) : setVal('')
+// 							}
+// 							} />
+// 								<div className={style.buttonsIcon}>
+// 						<button id='kb' className={style.inputButton}>
+// 							<img src={keyboard} alt="" className={style.img} />
+// 						</button>
+// 						<button id='mph' className={style.inputButton}>
+// 							<img src={microphone} alt="" className={style.img} />
+// 						</button>
+// 					</div>
+// 					</div>
+// 				</div>
+// 				<div className={style.buttons}>
+// 					<button className={style.button}>Посик в Google</button>
+// 					<button className={style.button}>Мне повезет</button>
+// 				</div>
+// 			</div>
+// 		</>
+// 	)
+// }
 
 export default Body
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Body from './Body';
-// import style from './style.module.css'
 import Footer from './Footer'
 
-const Main:React.FC = () =>{
+
+
+const Main:React.FC<any> = () =>{
 	return(
 		<>
 			<Header/>
